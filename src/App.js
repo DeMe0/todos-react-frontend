2e52f94bce512b0fb7384cb96c08edddbb99f900
Anthony DeMeo <anthonydemeo@anthonys-mbp.home>
2021-09-08 10:38:48 -0400

    first commit pushing to git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import AllPosts from "./pages/AllPosts";
+import SinglePost from "./pages/SinglePost";
+import Form from "./pages/Form";
+
+import React, {useState, useEffect} from "react";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
+
+
+  //////////////////////
+  // style objects
+  //////////////////////
+  const h1 = {
+    textAlign: "center",
+    margin: "10px"
+  }
+
+  /////////////////////////
+  // State & Other Variables
+  /////////////////////////
+
+  // API URL
+  const url = "https://am-628-django-todos.herokuapp.com/todos/"
+
+  // State to Hold List of Todos
+  const [posts, setPosts] = useState([])
+
+  //////////////////////////
+  // Functions
+  //////////////////////////
+
+  const getTodos = async () => {
+    const response = await fetch(url) // fetches data
+    const data = await response.json() // presents it as json
+    setPosts(data) 
+  }
+  // now we're going to need to use useEffect to call this function when it first loads
+  
+  //////////////////////////
+  // useEffects
+  //////////////////////////
+  useEffect(() => {getTodos()}, []) // need the array to make sure we don't end up in an infinite loop
+
+  //////////////////////////////
+  // Returned JSX
+  //////////////////////////
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={h1}> My Todo List </h1>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(routerProps) => <AllPosts {...routerProps} posts={posts} />}
+        />
+        <Route
+          path="/post/:id"
+          render={(routerProps) => <SinglePost {...routerProps} posts={posts} />}
+        />
+        <Route
+          path="/new"
+          render={(routerProps) => <Form {...routerProps}/>}
+        />
+        <Route
+          path="/edit"
+          render={(routerProps) => <Form {...routerProps}/>}
+        />
+      </Switch>
     </div>
   );
 }
